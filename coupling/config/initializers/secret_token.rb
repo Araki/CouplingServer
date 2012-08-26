1b244bea4445f3a2b7b5530d6d41d7f06e2fd025
Naoyuki Yamada <n.yamada@cyberagentamerica.com>
2012-08-26 16:00:26 +0900

    railsプロジェクト作成

---
+++ coupling/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coupling::Application.config.secret_token = 'e15c36a42266356228f4627b6aff789ba378216c528cdbfb8b5bcef129820b97cf7d2bbb4d2cd6457c0a066fda4bb4bdb30449082320bff376a4fc7a794db6a1'
