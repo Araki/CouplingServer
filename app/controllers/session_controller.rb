dbee939773178ccaac48a8ec3dc82bc03086676b
Naoyuki Yamada <n.yamada@cyberagentamerica.com>
2012-09-09 20:54:56 +0900

    index, session, userの各コントローラについて、ベースのメソッドと戻り値を追加

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,28 @@
 class SessionController < ApplicationController
   def register
-    render :text => 'this is session/register.'
+    if params[:facebook_id] == '10001' && params[:access_token] == 'access_token'
+      @return = {:success => :true, 
+                 :login_token => 'xxxxx', 
+                 :session_id => 'yyyyy'}
+    else
+      @return = {:success => :false}
+    end
+    render :json => @return
   end
 
   def create
+    if params[:facebook_id] == '10001' && params[:login_token] == 'login_token'
+      @return = {:success=> :true, 
+                 :session_id => 'xxxxx'}
+    else
+      @return = {:success => :false}
+    end
+    render :json => @return
   end
 
   def destroy
+    reset_session
+    @return = {:success => :true}
+    render :json => @return
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,19 +1,76 @@
 class UserController < ApplicationController
-  def profile
+  def profile_get
+    @return = {:success => true,
+               'user-123456789'=> {:favebook_id=>'123456789',
+                                   :gender=>'female',
+                                   :birthday=>'1979/7/19',
+                                   :hobby=>'123,147,156'}}
+    render :json => @return
+  end
+
+  def profile_post
+    render :nothing => true, :status => 202
   end
 
   def list
+    @return = {:success => true,
+                "user-12345" => {:something => 'xxxxx'},
+                "user-12346" => {:something => 'xxxxx'},
+                "user-12347" => {:something => 'xxxxx'},
+                "user-12348" => {:something => 'xxxxx'},
+                "user-12349" => {:something => 'xxxxx'}
+              }
+    render :json => @return
   end
 
-  def like
+  def like_get
+    @return = {:success => true,
+                "user-12345" => {:something => 'xxxxx'},
+                "user-12346" => {:something => 'xxxxx'},
+                "user-12347" => {:something => 'xxxxx'},
+                "user-12348" => {:something => 'xxxxx'},
+                "user-12349" => {:something => 'xxxxx'}
+              }
+    render :json => @return
+  end
+
+  def like_post
+    render :nothing => true, :status => 202
   end
 
   def likelist
+    @return = {:success => true,
+                "user-12345" => {:something => 'xxxxx'},
+                "user-12346" => {:something => 'xxxxx'},
+                "user-12347" => {:something => 'xxxxx'},
+                "user-12348" => {:something => 'xxxxx'},
+                "user-12349" => {:something => 'xxxxx'}
+              }
+    render :json => @return
   end
 
-  def block
+  def block_get
+
   end
 
-  def talk
+  def block_post
+    render :nothing => true, :status => 202
+  end
+
+  def blocklist
+  end
+
+  def talk_get
+    @return => {
+                'talk-2' => {:facebook_id => 12345678,
+                             :message => 'こんにちは'},
+                'talk-1' => {:facebook_id => 12345678,
+                             :message => 'はじめまして'}
+               }
+    render :json => @return
+  end
+
+  def talk_post
+    render :nothing => true, :status => 202
   end
 end
