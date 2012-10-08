1fa9ab701cf0486b389a535fd1f7eeaa97e8dc1c
Yuya Amano <Yuya@Yuya-no-MacBook-Air.local>
2012-10-08 15:34:57 +0900

    apn_on_railsのインストールとmigration完了

---
+++ config/configatron/test.rb
@@ -0,0 +1,24 @@
+# Override your default settings for the Test environment here.
+# 
+# Example:
+#   configatron.file.storage = :local
+
+# development (delivery):
+configatron.apn.passphrase => ''
+configatron.apn.port => 2195
+configatron.apn.host => 'gateway.sandbox.push.apple.com'
+configatron.apn.cert => File.join(RAILS_ROOT, 'config', 'aps_development.pem')
+
+# production (delivery):
+configatron.apn.host => 'gateway.push.apple.com'
+configatron.apn.cert => File.join(RAILS_ROOT, 'config', 'aps_development.pem')
+
+# development (feedback):
+configatron.apn.feedback.passphrase => ''
+configatron.apn.feedback.port => 2196
+configatron.apn.feedback.host => 'feedback.sandbox.push.apple.com'
+configatron.apn.feedback.cert => File.join(RAILS_ROOT, 'config', 'aps_development.pem')
+
+# production (feedback):
+configatron.apn.feedback.host => 'feedback.push.apple.com'
+configatron.apn.feedback.cert => File.join(RAILS_ROOT, 'config', 'aps_development.pem')
