f69b6b2dd61d449e9ac6a54e7f0707694a014535
uniom <hi.tsutti@gmail.com>
2013-01-06 19:35:06 +0900

    listメソッド実装

--- app/controllers/api/user/user_controller.rb
+++ app/controllers/api/user/user_controller.rb
@@ -1,8 +1,11 @@
 # coding:utf-8
 class Api::User::UserController < Api::User::BaseController
   def list
-    @users = User.limit(10)
-    render_ok(@users)
+    limit = params[:limit] ? params[:limit] : 10
+    offset = params[:offset] ? params[:offset] : 0
+    gender = @user.gender == 0 ? 1 : 0
+    users = User.where(gender: gender).offset(offset).limit(limit).map(&:to_hash)
+    render_ok(user: users)
   end
 
   def favorite_get
