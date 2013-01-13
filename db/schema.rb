7eca6a4060514ec855af41a85b5b7383086dfb39
uniom <hi.tsutti@gmail.com>
2013-01-13 20:56:32 +0900

    prefectureカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121223102552) do
+ActiveRecord::Schema.define(:version => 20130113115317) do
 
   create_table "apn_devices", :force => true do |t|
     t.string   "token",              :default => "", :null => false
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(:version => 20121223102552) do
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
     t.string   "login_token"
+    t.integer  "prefecture"
   end
 
 end
