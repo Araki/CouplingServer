89b0b35e163ba4e7b3cfebbd6b752308ed8b34e6
uniom <hi.tsutti@gmail.com>
2013-01-13 21:47:19 +0900

    editメソッド実装

--- app/controllers/api/user/profile_controller.rb
+++ app/controllers/api/user/profile_controller.rb
@@ -11,6 +11,28 @@ class Api::User::ProfileController < Api::User::BaseController
   end
 
   def edit
+    @user.prefecture   = params[:prefecture]
+    @user.introduction = params[:introduction]
+    @user.birthplace   = params[:birthplace]
+    @user.blood_type   = params[:blood_type]
+    @user.height       = params[:height]
+    @user.propotion    = params[:propotion]
+    @user.school       = params[:school]
+    @user.school_name  = params[:school_name]
+    @user.job          = params[:job]
+    @user.income       = params[:income]
+    @user.holiday      = params[:holiday]
+    @user.hobby        = params[:hobby]
+    @user.character    = params[:character]
+    @user.roommate     = params[:roommate]
+    @user.smoking      = params[:smoking]
+    @user.alcohol      = params[:alcohol]
+
+    if @user.save
+      render_ok
+    else
+      render_ng(@user.errors.full_messages)
+    end
     render_ok(@user.to_hash)
   end
 
