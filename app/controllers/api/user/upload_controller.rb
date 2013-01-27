1709b1c674bfd2296bfb3656ba8229879c214ea1
uniom <hi.tsutti@gmail.com>
2013-01-27 21:45:06 +0900

    画像アップロード処理修正

--- app/controllers/api/user/upload_controller.rb
+++ app/controllers/api/user/upload_controller.rb
@@ -9,17 +9,52 @@ class Api::User::UploadController < Api::User::BaseController
 
   def image_url
     entities = {
-      url: uploader(IMAGE_DATA_HASH).url(@user.id)
+      url: uploader(IMAGE_DATA_HASH).url(upload_key)
     }
     render_ok(entities)
   end
 
   def image_parameter
-    presinged_post = uploader(IMAGE_DATA_HASH).publish_url(@user.id)
+    presinged_post = uploader(IMAGE_DATA_HASH).publish_url(upload_key)
     entities = {
       url: presinged_post.url.to_s,
       fields: presinged_post.fields
     }
     render_ok(entities)
   end
+
+  private
+  def upload_key
+    type = params[:type] || "main"
+    order = params[:order] || 0
+
+    images = @user.images.order("order_number").find_all_by_type(model_type(type))
+    if images.present?
+      image = images[order.to_i]
+      if image
+        index = image.order_number
+      else
+        index = images.last.order_number + 1
+        create_image(type, index)
+      end
+    elsif
+      index = 0
+      create_image(type, index)
+    end
+
+    "#{type.downcase}_#{@user.id}_#{index}"
+  end
+
+  def model_type(type)
+    "#{type.capitalize}Image"
+  end
+
+  def create_image(type, index)
+    image = Image.new
+    image.user_id = @user.id
+    image.type = model_type(type)
+    image.order_number = index;
+    image.save!
+  end
+
 end
